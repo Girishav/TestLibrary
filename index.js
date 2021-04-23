16bc7fdcdf20138a3f28dfc368e80ad67449ad70
Girishav <girishavilam06@gmail.com>
2021-04-23 10:19:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import { NativeModules } from 'react-native';
+import ExampleComponent from "./src/ExampleComponent"
+
+const { RNTestLibrary } = NativeModules;
+
+export default {RNTestLibrary,ExampleComponent};
